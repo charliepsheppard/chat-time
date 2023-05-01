d220f3a0e017a318d403313f2dc4e2a092a92b31
Charlie Sheppard <sheppard.charlie24@gmail.com>
2023-05-01 09:24:15 -0700

    Add post request to add new user to DB

--- api/index.js
+++ api/index.js
@@ -5,15 +5,17 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const User = require('./models/User');
 
+
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
+app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL
-}))
+  origin: process.env.CLIENT_URL,
+}));
 
 app.get('/test', (req, res) => {
   res.json('test ok');
@@ -21,11 +23,16 @@ app.get('/test', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const {username, password} = req.body;
-  const createdUser = await User.create({username, password});
-  jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
+  try {
+    const createdUser = await User.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).status(201).json('ok');
+    });
+  } catch(err) {
     if (err) throw err;
-    res.cookie('token', token).status(201).json('ok');
-  });
+    res.status(500).json('error');
+  }
 });
 
 app.listen(4040);
